200a3899acce70f247102aa3664aa6af381f6567
Patrick Boos <patrickboos85@gmail.com>
2023-12-14 15:31:09 +0100

    solution of day 10 - part2

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -15,18 +15,157 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val map = input.map { it.toCharArray().toList() }
+        val startPosition = getStartPosition(map)
+        val mover = getConnectingPositions(map, startPosition)
+            .map { Mover(it, listOf(startPosition)) }
+            .mapNotNull { it.moveUntilClosedLoop(map) }
+            .minBy { it.pastPositions.size }
+
+        val MARKER_INSIDE = 'I'
+        val MARKER_OUTSIDE = 'O'
+        val MARKER_PIPE = '█'
+        val highResPipes = mapOf(
+            '.' to listOf(
+                "...".toCharArray(),
+                "...".toCharArray(),
+                "...".toCharArray(),
+            ),
+            'S' to listOf(
+                "███".toCharArray(),
+                "███".toCharArray(),
+                "███".toCharArray(),
+            ),
+            'F' to listOf(
+                "...".toCharArray(),
+                ".██".toCharArray(),
+                ".█.".toCharArray(),
+            ),
+            '|' to listOf(
+                ".█.".toCharArray(),
+                ".█.".toCharArray(),
+                ".█.".toCharArray(),
+            ),
+            'L' to listOf(
+                ".█.".toCharArray(),
+                ".██".toCharArray(),
+                "...".toCharArray(),
+            ),
+            '-' to listOf(
+                "...".toCharArray(),
+                "███".toCharArray(),
+                "...".toCharArray(),
+            ),
+            'J' to listOf(
+                ".█.".toCharArray(),
+                "██.".toCharArray(),
+                "...".toCharArray(),
+            ),
+            '7' to listOf(
+                "...".toCharArray(),
+                "██.".toCharArray(),
+                ".█.".toCharArray(),
+            ),
+            MARKER_INSIDE to listOf(
+                "III".toCharArray(),
+                "III".toCharArray(),
+                "III".toCharArray(),
+            ),
+            MARKER_OUTSIDE to listOf(
+                "OOO".toCharArray(),
+                "OOO".toCharArray(),
+                "OOO".toCharArray(),
+            ),
+        )
+
+        val detailedMap = Array(map.size * 3) { y -> Array(map[0].size * 3) { x -> '.'} }
+        fun fillDetailedMap(smallX: Int, smallY: Int, fillChar: Char) {
+            val bigX = smallX * 3
+            val bigY = smallY * 3
+            for (x in bigX until bigX + 3) {
+                for (y in bigY until bigY + 3) {
+                    val smallResXMod = x % 3
+                    val smallResYMod = y % 3
+                    val highResPipe = highResPipes[fillChar]
+                    detailedMap[y][x] = highResPipe!![smallResYMod][smallResXMod]
+                }
+            }
+        }
+
+        fun positionIsPipe(smallPosition: Position): Boolean {
+            val bigX = smallPosition.x * 3
+            val bigY = smallPosition.y * 3
+            for (x in bigX until bigX + 3) {
+                for (y in bigY until bigY + 3) {
+                    if (detailedMap[y][x] == MARKER_PIPE) {
+                        return true
+                    }
+                }
+            }
+            return false
+        }
+
+        fun isOutsideOfDetailedMap(position: Position) =
+            position.x < 0 || position.y < 0 || position.x > detailedMap[0].size - 1 || position.y > detailedMap.size - 1
+
+        fun findWayOut(position: Position): List<Position> {
+            val visitedPositions = mutableSetOf(position)
+            var traces = listOf(listOf(position))
+            while (true) {
+                if (traces.isEmpty()) return emptyList()
+
+                traces = traces.flatMap {trace ->
+                    val currentPosition = trace.last()
+                    val newPositions = listOf(
+                        Position(currentPosition.x - 1, currentPosition.y),
+                        Position(currentPosition.x + 1, currentPosition.y),
+                        Position(currentPosition.x, currentPosition.y - 1),
+                        Position(currentPosition.x, currentPosition.y + 1),
+                    )
+                        .filter { !visitedPositions.contains(it) }
+                        .filter { isOutsideOfDetailedMap(it) || detailedMap[it.y][it.x] != MARKER_PIPE }
+                    if (newPositions.any { isOutsideOfDetailedMap(it) || detailedMap[it.y][it.x] == MARKER_OUTSIDE }) {
+                        return trace
+                    }
+                    visitedPositions.addAll(newPositions)
+                    newPositions.map { trace + it }
+                }
+            }
+        }
+
+        mover.pastPositions.forEach { position ->
+            val (x, y) = position
+            val fillChar = map[y][x]
+            fillDetailedMap(x, y, fillChar)
+        }
+
+        val insidePositions = mutableListOf<Position>()
+        for (smallY in map.indices) {
+            for(smallX in map[smallY].indices) {
+                if (positionIsPipe(Position(smallX, smallY))) {
+                    continue
+                }
+                val wayOutTrace = findWayOut(Position(smallX * 3, smallY * 3))
+                if (wayOutTrace.isNotEmpty()) {
+                    fillDetailedMap(smallX, smallY, MARKER_OUTSIDE)
+                    wayOutTrace.forEach { detailedMap[it.y][it.x] = MARKER_OUTSIDE }
+                } else {
+                    fillDetailedMap(smallX, smallY, MARKER_INSIDE)
+                    insidePositions.add(Position(smallX, smallY))
+                }
+            }
+        }
+
+        return insidePositions.size
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput(10, isTest = true)
-    check(part1(testInput) == 4)
-    check(part1(readInput(10, part = 2, isTest = true)) == 8)
-//    check(part2(testInput) == 2)
+    check(part1(readInput(10, isTest = true)) == 4)
+    check(part2(readInput(10, part = 2, isTest = true)) == 8)
 
     val input = readInput(10)
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
 
 data class Position(val x: Int, val y: Int)
